e71fd331f74a3f59673609f1a82675b6806067fe
Janderson Lira <devjanderlira@gmail.com>
2026-02-04 16:17:28 -0400

    style: reformata eslint config e geojson via prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,16 +6,22 @@ export default withNuxt({
     // Vue
     'vue/multi-word-component-names': 'off',
     'vue/no-multiple-template-root': 'off',
-    'vue/html-self-closing': ['error', {
-      html: { void: 'always', normal: 'always', component: 'always' }
-    }],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: { void: 'always', normal: 'always', component: 'always' }
+      }
+    ],
 
     // TypeScript
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['error', {
-      argsIgnorePattern: '^_',
-      varsIgnorePattern: '^_'
-    }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
+    ],
 
     // Geral
     'no-console': ['warn', { allow: ['warn', 'error'] }],
